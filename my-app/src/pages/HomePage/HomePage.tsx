1ae8f1d0e990de4d379e1a820276408e7114898f
Phan Mạnh Dũng <phanmanhdung2k3@gmail.com>
2025-07-12 20:36:06 +0700

    build component react slick

--- my-app/src/pages/HomePage/HomePage.tsx
+++ my-app/src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import styles from './Home.module.scss';
 import classNames from 'classnames/bind';
 
 import SidebarMenu from '../../components/feature/SidebarMenu/SidebarMenu';
+import SlickSlider from '../../components/feature/SlickSliderComponent/SlickSliderComponent';
 
 const cx = classNames.bind(styles);
 
@@ -12,10 +13,16 @@ const HomePage: React.FC = () => {
       <div className="grid grid-cols-12 gap-4">
         <div className={`${cx('customLeft')} col-span-1`}>
           <div className={cx('wrapper_ul')}>
+            {/* Sidebar */}
             <SidebarMenu />
           </div>
         </div>
-        <div className={`${cx('customRight')} col-span-11`}>Nội dung bên phải</div>
+        <div className={`${cx('customRight')} col-span-11`}>
+          {/* Slick Slider */}
+          <div>
+            <SlickSlider />
+          </div>
+        </div>
       </div>
     </div>
   );
