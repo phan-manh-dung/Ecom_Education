70413d03ae8c1b7c229bddd95bf07e4e6152b86a
Phan Mạnh Dũng <phanmanhdung2k3@gmail.com>
2025-07-17 09:46:42 +0700

    fix sass and readme

--- my-app/src/pages/IndividualPage/IndividualPage.tsx
+++ my-app/src/pages/IndividualPage/IndividualPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { Row, Col, Empty, Modal, Spin } from 'antd';
 import toast from 'react-hot-toast';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
